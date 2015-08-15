4310f2cfb627c4b406549ceb456c82d4d9817362
tommottom <fb53ice4j@gmail.com>
2015-08-15 16:29:38 +0900

    変更

--- while_heisei.rb
+++ while_heisei.rb
@@ -8,6 +8,14 @@ while ad >= 1989
 end
 
 この場合条件文が真のため、無限ループとなります。
+ではどうすれば。。。楽しいるrubyにあとで書いてるかな
+
+suuzi=1
+while true
+puts(suuzi)
+break if suuzi==100
+suuzi+=1
+end
 
 
 ad = ARGV[0].to_i
@@ -15,4 +23,5 @@ heisei = ad -1989
 
 while ad >= 1989
   puts heisei
+break if ad = "stop"
 end
---
+++ while_test.rb
@@ -0,0 +1,7 @@
+ad = ARGV[0].to_i
+heisei = ad -1989
+
+while ad >= 1989
+  puts heisei
+break if
+end
