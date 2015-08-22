fbc4520c192481639ebf147bf28a18dcb3bed58a
tommottom <fb53ice4j@gmail.com>
2015-08-23 08:31:15 +0900

    勉強になった

--- fizzbuzz2.rb
+++ fizzbuzz2.rb
@@ -3,5 +3,11 @@
   puts "Fizz" if i % 3 == 0
   puts "Buzz" if i % 5 == 0
   puts i
-
 end
+
+
+=begin
+
+fizzの出力の段階で対応する値を、表示しないようにする。
+
+=end
--- initialize.rb
+++ initialize.rb
@@ -11,46 +11,13 @@ class Car
   def color
     @color
   end
+
+  def name_and_color
+    [@name, @color]
+  end
 end
 
 my_car = Car.new("プリウス", "白")
-puts my_car.name.color
+puts my_car.name
 puts my_car.color
-
-
-
-=begin
-class Car
-  def initialize(name, color)
-    @name = name
-    @color = color
-  end
-
-  def name
-    @name
-  end
-
-  def color
-    @color
-  end
-end
-
-my_car = Car.new("プリウス", "白")
-puts my_car.name.color
-
-
-
-my_car.name.color
-
-は
-
-my_car のインスタンスメソッド nameを実行した結果に対して、colorメソッドを実行した結果
-
-を表すコードになっており、my_car のインスタンスメソッド nameの実行結果は、
-"プリウス"という文字列なので、"プリウス"という文字列に colorメソッドを実行
-しようとして、質問にあるように
-
-"プリウス"という文字列には color というメソッドはありません。
-
-というエラーメッセージが表示されます。
-=end
+puts my_car.name_and_color
